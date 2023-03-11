4b3fb0c7d8d2e4c18a79daca6e714a4edcba02ad
ellisrgrz <90063530+ellisrgrz@users.noreply.github.com>
2023-03-10 16:11:11 -0800

    Update index.js
    I added index.js to determine substitutability with embedded js.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+const inp_scrip = document.getElementById("inputfile")
+const prnt_scrip = document.getElementById("print")
+inp_scrip.addEventListener('change', function(){
+    //prnt.innerHTML = inp.files[0].name
+    //console.log(inp.files[0].name)
+    prnt_scrip.innerHTML = "The external script works."
+    console.log("The external script works")
+})
